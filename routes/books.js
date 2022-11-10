6d09af3221c48cd6c53df925edeaa37e7ebfa706
ahmalla <ahmalla@hotmail.com>
2022-11-10 12:11:30 -0800

    added new book route/controller

--- routes/books.js
+++ routes/books.js
@@ -1,20 +1,36 @@
 var express = require("express");
 var router = express.Router();
 const Book = require("../models/book");
+const Author = require("../models/author")
 
 // all books route
 router.get("/", async (req, res) => {
+    res.send("All Books")
     
     
 });
 // New book Route
-router.get('/new', (req, res) => {
-   
+router.get('/new', async (req, res) => {
+   try {
+    // get all authors
+    const authors = await Author.find({})
+    // create new book
+    const book = new Book()
+    // if no error
+    res.render("books/new", {
+        authors: authors,
+        book: book
+    })
+   } catch {
+    // if error redirect to books page
+    res.redirect("/books")
+
+   }
 });
 
 // create new books route
 router.post("/", async (req, res) => {
-    
+    res.send("Create Book")
 });
 
 module.exports = router;
