521b30cce79d2b219cdf04997e99eaa48aadb380
ahmalla <ahmalla@hotmail.com>
2022-11-10 07:45:48 -0800

    added author model and author folder in views

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const Author = require("../models/author");
 
 // all authors route
 router.get("/", (req, res) => {
@@ -7,7 +8,7 @@ router.get("/", (req, res) => {
 });
 // New Author Route(displaying the form)
 router.get('/new', (req, res) => {
-    res.render('authors/new')
+    res.render('authors/new', { author: new Author() })
 });
 
 // creates new authors route
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ require("./config/database");
 require("./config/passport");
 
 var indexRouter = require("./routes/index");
+var authorRouter = require("./routes/authors");
 var usersRouter = require("./routes/users");
 
 var app = express();
@@ -49,6 +50,7 @@ app.use(function (req, res, next) {
 
 // ROUTES
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
