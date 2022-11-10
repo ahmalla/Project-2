82ae8ec7f97df661e19485d23616513d2c8cca2e
ahmalla <ahmalla@hotmail.com>
2022-11-10 08:56:14 -0800

    converting create author to async

--- routes/authors.js
+++ routes/authors.js
@@ -12,24 +12,29 @@ router.get('/new', (req, res) => {
 });
 
 // creates new authors route
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     const author = new Author({
         // explicitly passing name so the user cant enter something else and reset things, like id
         name: req.body.name
     })
-    // saving new authors calling save method on author object
-    author.save((err, newAuthor) => {
-        if (err) {
-            // if error render new page passing params on author page(handles dupes)
-            res.render("authors/new", {
-                author: author, 
-            })
-        } else {
-            // if no error bring back to author page
-            // res.redirect(`authors/${newAuthor.id}`)
-            res.redirect('authors')
-        }
-    })
+    try {
+        const newAuthor = await author.save()
+        res.redirect(`authors/${newAuthor.id}`)
+        res.redirect('authors')
+    } catch {
+        res.render("authors/new", {
+            author: author, 
+        })
+    }
+    // // saving new authors calling save method on author object
+    // author.save((err, newAuthor) => {
+    //     if (err) {
+    //         // if error render new page passing params on author page(handles dupes)
+    //         
+    //     } else {
+    //         // if no error bring back to author page
+    //         
+    // })
 });
 
 module.exports = router;
