d6a777b16847962f7a803747e6e2a496ca8ba9b7
ahmalla <ahmalla@hotmail.com>
2022-11-10 08:22:52 -0800

    working on create author route

--- routes/authors.js
+++ routes/authors.js
@@ -13,7 +13,23 @@ router.get('/new', (req, res) => {
 
 // creates new authors route
 router.post("/", (req, res) => {
-    res.send("Create")
+    const author = new Author({
+        // explicitly passing name so the user cant enter something else and reset things, like id
+        name: req.body.name
+    })
+    // saving new authors calling save method on author object
+    author.save((err, newAuthor) => {
+        if (err) {
+            // if error render new page passing params on author page(handles dupes)
+            res.render("authors/new", {
+                author: author, 
+            })
+        } else {
+            // if no error bring back to author page
+            // res.redirect(`authors/${newAuthor.id}`)
+            res.redirect('authors')
+        }
+    })
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded);
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 // Method-Override
 app.use(methodOverride("_method"));
